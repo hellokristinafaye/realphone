838bbf26d0c9c18fbca78f871eb032e56529581c
Kristina Faye <hellokristinafaye@gmail.com>
2025-02-03 14:45:58 -0800

    downBtn now rotates the circle

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ var rotateValue = circle.style.transform;
 var rotateSum;
 
 upBtn.onclick = function () { 
+    // this line changes the value of the transform property by -90deg
     rotateSum = rotateValue + "rotate(-90deg)";
+    // this line updates the transform property to rotateSum 
     circle.style.transform = rotateSum;
+    // this line updates rotateValue to be teh same as rotateSum
+    rotateValue = rotateSum
+}
+downBtn.onclick = function () { 
+    // this line changes the value of the transform property by -90deg
+    rotateSum = rotateValue + "rotate(90deg)";
+    // this line updates the transform property to rotateSum 
+    circle.style.transform = rotateSum;
+    // this line updates rotateValue to be teh same as rotateSum
     rotateValue = rotateSum
 }
